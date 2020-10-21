b0fc28dd6490d4afe838f0dedd95af7a287395fc
su <2296887348@qq.com>
2020-10-21 20:43:45 +0800

    egg.js项目起步

---
+++ cloud-disk-api/app/controller/home.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+    // eslint-disable-next-line quotes
+    ctx.body = "hello world";
+  }
+
+
+  async list() {
+    this.ctx.body = {
+      msg: 'ok',
+      data: [
+        {
+          name: '微服务',
+          price: 100,
+        },
+        {
+          name: 'Java',
+          price: 88,
+        },
+        {
+          name: 'JavaScript',
+          price: 77,
+        },
+      ],
+    };
+  }
+}
+
+module.exports = HomeController;
