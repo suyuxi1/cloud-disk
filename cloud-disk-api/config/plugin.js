d681d405f02d940868c25e3718a6a6ccbc6e28f2
su <2296887348@qq.com>
2020-10-23 15:34:29 +0800

    redis缓存插件和封装

--- cloud-disk-api/config/plugin.js
+++ cloud-disk-api/config/plugin.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /** @type Egg.EggPlugin */
 module.exports = {
@@ -18,4 +18,8 @@ module.exports = {
     enable: true,
     package: 'egg-valparams',
   },
-};
+  redis: {
+    enable: true,
+    package: 'egg-redis',
+  },
+}
