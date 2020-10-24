849ff2c0794fc2459699b6d1ef2612438ffe8542
su <2296887348@qq.com>
2020-10-24 19:05:17 +0800

    搜索文件

--- cloud-disk-api/app/controller/file.js
+++ cloud-disk-api/app/controller/file.js
@@ -240,6 +240,34 @@ class FileController extends Controller {
     }
     ctx.apiSuccess(res)
   }
+
+  //搜索文件
+  async search() {
+    const { ctx, app } = this
+    const user_id = ctx.authUser.id
+
+    ctx.validate({
+      keyword: {
+        required: true,
+        type: 'string',
+        desc: '关键字',
+      },
+    })
+    let { keyword } = ctx.query
+    const Op = app.Sequelize.Op
+    let rows = await app.model.File.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${keyword}%`,
+        },
+        isdir: 0,
+        user_id,
+      },
+    })
+    ctx.apiSuccess({
+      rows,
+    })
+  }
 }
 
 module.exports = FileController
